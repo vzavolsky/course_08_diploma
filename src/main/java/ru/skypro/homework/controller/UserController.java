fbafae0acf378ec0291f677b80b161f95611ccae
Valery <vzavolsky@ya.ru>
2023-08-23 20:29:17 +0300

    UserController

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -1,4 +1,23 @@
 package ru.skypro.homework.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@RestController
+@CrossOrigin(value = "http://localhost:3000")
 public class UserController {
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getUser() {
+        return ResponseEntity.ok().build();
+    }
+
 }
